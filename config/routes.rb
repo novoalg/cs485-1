ae52bd6c2bf38b2898dc2248b0d8109af350a4fa
Ben Christianson <thhuntertgm@gmail.com>
2014-10-02 20:21:11 -0500

    Created about us and contact pages.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
 
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
