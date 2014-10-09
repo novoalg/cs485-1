00eb01985003b8444c81a520135ac57589df1ad3
Ben Christianson <thhuntertgm@gmail.com>
2014-10-09 16:50:37 -0500

    Fixed slick integration.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require slick
 //= require foundation
 //= require turbolinks
 //= require_tree .
-//= require slick
 
 $(function(){ $(document).foundation(); });
---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,9 @@
+$('.welcomeSlider').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
