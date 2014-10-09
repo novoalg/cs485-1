9a70f2faca4555322a7c3c656d1e1471d3cd1742
Lucas Novoa <novoalg@uwec.edu>
2014-10-09 16:54:04 -0500

    Fixed sessions, added user form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_username(params[:session][:username])
-    
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       current_user = user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,33 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create user_params
+    if @user.save
+        flash[:success] = "Welcome #{@user.username}"
+        session[:user_id] = @user.id
+        current_user = @user
+        redirect_to root_path
+    else
+        flash[:notice] = @user.errors.full_messages.to_sentence
+        render 'new'
+        @user.destroy
+    end
   end
 
   def edit
+    @user = User.find params[:id]
   end
 
   def update
   end
 
+  private
+    
+    def user_params
+        params.require(:user).permit!
+    end
 end
