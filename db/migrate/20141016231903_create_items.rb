aa9d4492cac63f5c47a27eca98e35ec37d20c2ed
Ben Christianson <thhuntertgm@gmail.com>
2014-10-16 19:40:52 -0500

    Stubbed migrations

--- db/migrate/20141016231903_create_items.rb
+++ db/migrate/20141016231903_create_items.rb
@@ -2,6 +2,12 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
 
+      t.string :name
+      t.text :description
+      t.decimal :price, :precision => 8, :scale => 2
+
+      t.integer :in_stock
+
       t.timestamps
     end
   end
