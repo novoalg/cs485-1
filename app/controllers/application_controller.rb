fc95e0097bb6491db6049bd4bbf901733259c69c
Ben Christianson <thhuntertgm@gmail.com>
2014-10-30 20:31:04 -0500

    Application controller work now included

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,39 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  def role_zero
+    unless (user_signed_in? && current_user.role_id >= 0)
+      flash[:notice] = "You don't have permission to the page you tried to access."
+      redirect_to root_path
+    end
+  end
+
+  def role_one
+    unless (user_signed_in? && current_user.role_id >= 1)
+      flash[:notice] = "You don't have permission to the page you tried to access."
+      redirect_to root_path
+    end
+  end
+
+  def role_two
+    unless (user_signed_in? && current_user.role_id >= 2)
+      flash[:notice] = "You don't have permission to the page you tried to access."
+      redirect_to root_path
+    end
+  end
+
+  def role_three
+    unless (user_signed_in? && current_user.role_id >= 3) 
+      flash[:notice] = "You don't have permission to the page you tried to access."
+      redirect_to root_path
+    end
+  end
+
+  def role_four
+    unless (user_signed_in? && current_user.role_id == 4)
+      flash[:notice] = "You don't have permission to the page you tried to access."
+      redirect_to root_path
+    end
+  end
+
 end
