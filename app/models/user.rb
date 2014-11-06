37dffb86a39cfcbafff7515c2c26cf64cfb036d1
delebota <delebota@uwec.edu>
2014-11-06 13:55:11 -0800

    Model Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   belongs_to :role
 
   validates :username, presence: true, length: { maximum: 15 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, :length => {:within => 6..20}
   validates :password_digest, presence: true
   validates_presence_of :role_id
   validates_inclusion_of :role_id, :in => 0..4
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,9 +7,22 @@ class UserTest < ActiveSupport::TestCase
   end
 
   describe "Create User" do
-      user = User.new(:email => 'user@example.com', :username => 'user', 
-                :password => 'password', :password_confirmation => 'password')
+      user = User.new(:email => "user5@example.com", :username => "user5", 
+                :role_id => 1, :password => 'password', :password_confirmation => 'password')
       user.valid?.should == true
+  end
+
+  describe "Save User" do
+      user = User.create(:email => "user6@example.com", :username => "user6", 
+                :role_id => 1, :password => 'password', :password_confirmation => 'password')
+      user.valid?.should == true
+
+      user2 = User.find(user.id)
+      (user == user2).should == true
+  end
+
+  describe "Delete User" do
+      user = User.find_by(:email => "user6@example.com").destroy
   end
 
 end
