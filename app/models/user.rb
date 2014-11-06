02defe34b18ac9cb88bcaf5ad5491e599c10d1e1
Ben Christianson <thhuntertgm@gmail.com>
2014-11-06 16:16:54 -0600

    Fixed user inconsistencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,7 @@ class User < ActiveRecord::Base
   VALID_USERNAME_REGEX = /\A[a-z0-9\-]*\z/i
   belongs_to :role
 
-  validates :username, presence: true, length: { maximum: 15 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password_digest, presence: true
-  validates_presence_of :role_id
-  validates_inclusion_of :role_id, :in => 0..4
+  validates_uniqueness_of :email
   
   before_save do
     self.email = email.downcase
