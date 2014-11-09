910acaf06af5891ff08b23d21e33f146931f072b
Ben Christianson <thhuntertgm@gmail.com>
2014-11-09 03:04:51 -0600

    Don't send emails just yet

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
-after_create :send_email
+# after_create :send_email
 
   # GET /resource/sign_up
   # def new
