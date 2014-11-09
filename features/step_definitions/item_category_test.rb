45b0da5357df436f7e1a97f3f4870b80033a08ca
Ben Christianson <thhuntertgm@gmail.com>
2014-11-08 23:38:34 -0600

    Item categories tests

--- features/step_definitions/item_category_test.rb
+++ features/step_definitions/item_category_test.rb
@@ -1,11 +1,34 @@
-Given(/^I am on the item category page$/) do
-  unless current_path == item_categories_path
-    visit item_categories_path
-  end
+When(/^I go to the new category page$/) do
+  visit new_item_category_path
 end
 
-Given(/^I am on the new item category page$/) do
-  unless current_path == new_item_category_path
-    visit new_item_category_path
-  end
+When(/^I enter Things as the category name$/) do
+  @sr = SecureRandom.hex
+  fill_in "name", :with => "Things#{@sr}"
+end
+
+When(/^I enter Stuff as the category description$/) do
+  fill_in "description", :with => "Stuff"
+end
+
+When(/^I save the category$/) do
+  click_button "Submit"
+end
+
+Then(/^I should see Things in the item category list$/) do
+  expect(page).to have_content "Things#{@sr}"
+end
+
+When(/^I view the Things category$/) do
+  visit item_category_path ItemCategory.last.id
+end
+
+Then(/^I should see the category name$/) do
+  expect(page).to have_content "Things#{@sr}"
+end
+
+When(/^I edit the Things category$/) do
+  visit edit_item_category_path ItemCategory.last.id
+  step "I enter Things as the category name"
+  step "I save the category"
 end
