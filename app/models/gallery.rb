75b10ea1b7c893c02ebfc29424ec26a2cc5ec4f0
Ben Christianson <thhuntertgm@gmail.com>
2014-11-30 20:13:48 -0600

    Changed gallery sizes.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,6 +3,6 @@ class Gallery < ActiveRecord::Base
   validates_presence_of :text
   validates_presence_of :picture
 
-  has_attached_file :picture, :styles => { :large => "3000x2000>", :medium => "1800x1200>", :small => "900x600>" }, :default_url => "missing/gallery_:style.jpg"
+  has_attached_file :picture, :styles => { :large => "3000x2000>", :medium => "600x400>", :small => "300x200>" }, :default_url => "missing/gallery_:style.jpg"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
