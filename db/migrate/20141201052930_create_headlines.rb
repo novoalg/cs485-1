44f0527c27c753f14070cab4db0701522678a6b8
Ben Christianson <thhuntertgm@gmail.com>
2014-12-01 00:23:19 -0600

    Default to false on headline create.

--- db/migrate/20141201052930_create_headlines.rb
+++ db/migrate/20141201052930_create_headlines.rb
@@ -2,7 +2,7 @@ class CreateHeadlines < ActiveRecord::Migration
   def change
     create_table :headlines do |t|
       t.text :text
-      t.boolean :active
+      t.boolean :active, default: false
       t.timestamps
     end
   end
