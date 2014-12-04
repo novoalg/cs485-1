436f211849fcbb2ba250514f0aa1c03ee9b348e1
Ben Christianson <thhuntertgm@gmail.com>
2014-12-03 22:45:08 -0600

    Can send emails is now based on role.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 Role.create(name: "None", description: "No Permissions - Average User")
 Role.create(name: "Read", description: "Read-only access to information")
 Role.create(name: "Edit", description: "Allowed to edit existing information")
-Role.create(name: "Add", description: "Allowed to create new items, categories, etc.")
+Role.create(name: "Add", description: "Allowed send emails and create new data.")
 Role.create(name: "Delete", description: "Allowed to remove information from the application.")
 
 EmailTemplate.create(
