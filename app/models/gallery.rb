1646b025a7a3c0db0a21a30d77786495b583c526
Ben Christianson <thhuntertgm@gmail.com>
2014-12-04 13:41:25 -0600

    300px thumbs

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,6 +3,6 @@ class Gallery < ActiveRecord::Base
   validates_presence_of :text
   validates_presence_of :picture
 
-  has_attached_file :picture, :styles => { :large => "3000x2000>", :medium => "1800x1200>", :thumb => "400x400>" }, :default_url => "missing/gallery_:style.jpg"
+  has_attached_file :picture, :styles => { :large => "3000x2000>", :medium => "1800x1200>", :thumb => "300x300>" }, :default_url => "missing/gallery_:style.jpg"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
