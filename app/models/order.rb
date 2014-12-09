c97a2504fa010936b1613ef602134c66e93606b3
Ben Christianson <thhuntertgm@gmail.com>
2014-12-08 21:08:58 -0600

    Emails...

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
   has_many :order_items
   belongs_to :user
+
+  def subtotal 
+    self.order_items.inject { | sum, x | sum + (x.price_per_unit * x.quantity) }
+  end
 end
